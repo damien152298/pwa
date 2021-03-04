a52a6e572989a0d79de1836d5ec91600b439bf45
Damien <damien.micallef98@gmail.com>
2021-03-04 13:15:02 +0100

    commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <header>
+      <h1>{{sitename}}</h1>
+      <button @click="showCheckout">{{ this.cart.length }}Checkout</button>
+    </header>
+  </div>
+</template>
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      sitename: "Vue.js Pet Depot",
+cart: [] }
+},
+  methods: { showCheckout() {} }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/ProductList.vue
@@ -0,0 +1,36 @@
+<template>
+  <main>
+    <div v-for="product in products" :key="product.id">
+      <h2>{{product.title}}</h2>
+      <figure>
+        <img v-bind:src="product.image" />
+      </figure>
+      <p v-html="product.description"></p>
+      <p>Price: {{ product.price }}</p>
+      <p>Available stock: {{ product.availableInventory }}</p>
+      <button @click="addToCart(product)">Add to cart</button>
+    </div>
+  </main>
+</template>
+<script>
+export default {
+  name: "ProductList",
+  data() {
+    return {
+      products: [
+        {
+          id: 1001,
+          title: "Cat Food, 25lb bag",
+          image: "product-fullsize.png",
+},
+      ]}}}
+
+      export default {
+  name: "ProductList",
+  data() {},
+  methods: {
+    addProduct(product) {
+      this.$emit('addProduct',product);
+}, },
+};
+</script>
